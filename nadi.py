8b4c48bfdb2123f531938c7bb4124fc0d675323a
Uwituze bernardine <bernardineuwituze6gmail.com>
2023-11-04 16:47:10 +0200

    initial commit

---
+++ nadi.py
@@ -0,0 +1,22 @@
+num1 = 20
+num2 = 10
+sum = num1 + num2
+result = int(sum)
+print(f"the sum of{num1} and {num2} is {result}")
+orginal_string ="bennie"
+reversed_string = print(orginal_string[ : :-1])
+num_str = "42"
+num_int = int(num_str)
+print(num_int)
+num_float = 3.14
+num_str = str(num_float)
+print(num_str)
+name = input("enter your name ")
+age = input("enter your age ")
+print(f"hello {name} ! you are {age} years old")
+string_name = "string shiffle"
+str1 = "string"
+str2 = "shiffle"
+
+print(str1[:-1]+str2[:-1])
+
